fd23e3c13f3fa593e524c7a55426afdf43154818
Sam Buchl <buchl.sam@gmail.com>
2021-02-14 11:14:04 -0600

    add base template and index basic blogging content

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Sam'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Frigid day in Mantorville!'
+        },
+        {
+            'author': {'username': 'Linda'},
+            'body': 'Busy day at Mayo!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
